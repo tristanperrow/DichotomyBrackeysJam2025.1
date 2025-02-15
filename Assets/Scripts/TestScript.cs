4708d4dcae708d9676fdf31baa814f9a92a456f9
Tristan Perrow <tsperrow49@gmail.com>
2025-02-15 18:04:09 -0500

    chore: initial push test 2

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +14,5 @@ public class TestScript : MonoBehaviour
     {
         
     }
+
 }
